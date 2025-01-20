8fd9e9b1fc87781f8d5c82dcf76533d353f529c2
sherm <shermawns@gmail.com>
2025-01-20 14:33:10 -0300

    first commit

---
+++ src/main/java/com/Park_Api/ParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.Park_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkApiApplication.class, args);
+	}
+
+}
